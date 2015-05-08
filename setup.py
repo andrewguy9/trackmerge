ea6dc7160c4af29aa70dae1e7e8b18525a8d869c
Andrew Thomson <athomsonguy@gmail.com>
2015-05-07 17:55:38 -0700

    fix version and init document

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='trackmerge',
-      version='0.1.0',
+      version='0.2.3',
       description='Tool for tracking project release versions with git.',
       url='https://github.com/andrewguy9/trackmerge',
       author='andrew thomson',
