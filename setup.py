fbb71c686cc57a0cd5be4ef0b2f7b9a773036051
Andrew Thomson <athomsonguy@gmail.com>
2015-07-24 12:14:53 -0700

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='trackmerge',
-      version='0.2.5',
+      version='0.2.6',
       description='Tool for tracking project release versions with git.',
       url='https://github.com/andrewguy9/trackmerge',
       author='andrew thomson',
